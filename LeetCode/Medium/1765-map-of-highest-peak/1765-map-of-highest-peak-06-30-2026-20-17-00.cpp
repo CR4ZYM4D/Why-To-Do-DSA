2ed8d49d447cc2901edcf5bec92d3cd4d17c3f49
Madhav Dixit <163180947+CR4ZYM4D@users.noreply.github.com>
2026-06-30 20:17:00 +0530

    1765-map-of-highest-peak Array .cpp

---
+++ LeetCode/Medium/1765-map-of-highest-peak/1765-map-of-highest-peak-06-30-2026-20-17-00.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& mat) {
+       
+       vector<int> rows= {-1, 1, 0 , 0};
+       vector<int> cols= {0, 0, -1 , 1};
+        int m = mat.size();
+        int n = mat[0].size();
+        vector<vector<int>> dist(m, vector<int>(n));
+        vector<vector<bool>> visited(m, vector<bool>(n));
+        deque<vector<int>> dq;
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(mat[i][j] == 1){
+                    dq.push_back({i, j});
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+        int steps = 1;
+
+        while(!dq.empty()){
+            
+            int l = dq.size();
+            while(l--){
+                auto arr = dq.front();
+                dq.pop_front();
+                for(int i=0; i<4; i++){
+                    int r = arr[0] + rows[i];
+                    int c = arr[1] + cols[i];
+
+                    if(r<0 || r>=m || c<0 || c>=n || visited[r][c]) continue;
+                    visited[r][c] = true;
+                    dist[r][c] = steps;
+                    dq.push_back({r, c});
+                }
+            }
+            steps++;
+        }
+        return dist;
+
+    }
+};
