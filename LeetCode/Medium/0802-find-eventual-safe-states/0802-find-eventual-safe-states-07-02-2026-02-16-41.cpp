1a970fa6c78444ad8397b7342d7a29c15291949e
Madhav Dixit <163180947+CR4ZYM4D@users.noreply.github.com>
2026-07-02 02:16:42 +0530

    0802-find-eventual-safe-states Depth-First Search .cpp

---
+++ LeetCode/Medium/0802-find-eventual-safe-states/0802-find-eventual-safe-states-07-02-2026-02-16-41.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        
+        int n = graph.size();
+        vector<vector<int>> adj(n, vector<int>());
+
+        vector<int> res;
+        vector<int> dq;
+        vector<int> inDegree(n, 0);
+
+        for(int i=0; i<n; i++){
+            for(int j: graph[i]){
+                adj[j].push_back(i);
+                inDegree[i]++;
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            if(inDegree[i]== 0)dq.push_back(i);
+        }
+
+        while(!dq.empty()){
+            int node = dq[0];
+            dq.erase(dq.begin());
+            res.push_back(node);
+            for(int i: adj[node]){
+                inDegree[i]--;
+                if(inDegree[i]== 0) dq.push_back(i);
+            }
+        }
+        std::sort(res.begin(), res.end());
+        return res;
+    }
+};
